4cc2660ce22218c4c437aeafe0d926b4760003b1
Lucas Henrique <lucasheriques@gmail.com>
2020-09-27 02:27:35 -0300

    Setting up Reactotron and local imports

--- App.tsx
+++ App.tsx
@@ -1,15 +1,17 @@
+import { RootStackList as StackRouteList } from "@constants/types";
 import { NavigationContainer, StackRouter } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import FinishScreen from "@screens/FinishScreen";
+import HomeScreen from "@screens/HomeScreen";
+import QuizScreen from "@screens/QuizScreen";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { Provider as PaperProvider } from "react-native-paper";
 
-import { RootStackList as StackRouteList } from "./src/constants/types";
-import FinishScreen from "./src/screens/FinishScreen";
-import HomeScreen from "./src/screens/HomeScreen";
-import QuizScreen from "./src/screens/QuizScreen";
-
 export default function App() {
+  if (__DEV__) {
+    import("./ReactotronConfig");
+  }
   const Stack = createStackNavigator<StackRouteList>();
   return (
     <NavigationContainer>
